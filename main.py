61dcbb24412666a6cacc3095df1ab0b5f760a876
Enkhbold470 <enkhbold470@gmail.com>
2024-04-20 16:26:33 -0700

    code for client

---
+++ main.py
@@ -0,0 +1,24 @@
+import os
+import requests
+from playsound import playsound
+
+
+
+# URL of the mp3 file
+url = "http://192.168.90.126:5000/speech"
+
+# Send GET request
+response = requests.get(url)
+
+# Save the mp3 file
+with open('speech.mp3', 'wb') as file:
+    file.write(response.content)
+
+# Play the mp3 file
+if os.path.isfile('speech.mp3'):
+        # Play the mp3 file
+        print("Playing the mp3 file...")
+        playsound('speech.mp3')
+else:
+        print("The file does not exist or is not valid.")
+
